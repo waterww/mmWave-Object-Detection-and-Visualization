5dc5c2606597373c19cba0efe819c7bcf91be4f9
waterww <45504345+waterww@users.noreply.github.com>
2020-12-19 04:24:28 +0800

    Update tf_broadcaster.cpp

--- learning_tf/src/tf_broadcaster.cpp
+++ learning_tf/src/tf_broadcaster.cpp
@@ -16,15 +16,15 @@ void poseCallback(const nav_msgs::Odometry odom){
   double x = odom.pose.pose.position.x;
   double y = odom.pose.pose.position.y;
 
-  //设置transform的平移参数
+  //set translation parameters
   odom_trans.transform.translation.x = x;
   odom_trans.transform.translation.y = y;
   odom_trans.transform.translation.z = 0.0;
 
-  //设置transform的转动参数
+  //set rotation parameters
   odom_trans.transform.rotation = odom.pose.pose.orientation;
 
-  //turtle_name在世界坐标系下的点坐标setOrigin,与世界坐标系之间的转角setRotation
+  //broadcast tf transform
   br.sendTransform(odom_trans);
   //1.transform 2.timestamp 3.name of parent frame 4.name of child frame
 
@@ -35,8 +35,9 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "tf_broadcaster");
 
   ros::NodeHandle node;
-
-  ros::Subscriber sub = node.subscribe("/integrated_to_init", 10, &poseCallback);//先订阅位姿消息再TF广播
+  
+  //subscribe odometry and broadcast tf transform
+  ros::Subscriber sub = node.subscribe("/integrated_to_init", 10, &poseCallback);
   
 
   std::cout << "Start boardcasting tf..." << std::endl;
